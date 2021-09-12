5229dd93d5a3dcbcd82950da839e72fdddcf1b3c
Barrek Roberts <b-roberts@users.noreply.github.com>
2021-09-12 08:46:05 -0500

    Improves Article Relations

---
+++ resources/js/graphdata.js
@@ -0,0 +1,51 @@
+var json = [  
+    {  
+      "adjacencies": [  
+          "graphnode21",   
+          {  
+            "nodeTo": "graphnode1",  
+            "nodeFrom": "graphnode0",  
+            "data": {  
+              "$color": "#557EAA"  
+            }  
+          }, {  
+            "nodeTo": "graphnode13",  
+            "nodeFrom": "graphnode0",  
+            "data": {  
+              "$color": "#909291"  
+            }  
+          }, {  
+            "nodeTo": "graphnode14",  
+            "nodeFrom": "graphnode0",  
+            "data": {  
+              "$color": "#557EAA"  
+            }  
+          }, {  
+            "nodeTo": "graphnode15",  
+            "nodeFrom": "graphnode0",  
+            "data": {  
+              "$color": "#557EAA"  
+            }  
+          }, {  
+            "nodeTo": "graphnode16",  
+            "nodeFrom": "graphnode0",  
+            "data": {  
+              "$color": "#557EAA"  
+            }  
+          }, {  
+            "nodeTo": "graphnode17",  
+            "nodeFrom": "graphnode0",  
+            "data": {  
+              "$color": "#557EAA"  
+            }  
+          }  
+      ],  
+      "data": {  
+        "$color": "#83548B",  
+        "$type": "circle",  
+        "$dim": 10  
+      },  
+      "id": "graphnode0",  
+      "name": "graphnode0"  
+    }  
+];  
--- resources/views/article/custom_index/group.blade.php
+++ resources/views/article/custom_index/group.blade.php
@@ -1,7 +1,20 @@
 <div class="card">
     <div class="card-body">
-       <h1>Members</h1>
-        @foreach($article->reverseConnections->where('pivot.relationship','MEMBER_OF') as $connection)
+        <h1>Leader</h1>
+        @foreach($article->reverseConnections->where('pivot.relationship','LEADER_OF') as $connection)
+            @include('modules.article_media',['article'=>$connection])
+        @endforeach
+        <h1>Champion</h1>
+        @foreach($article->reverseConnections->where('pivot.relationship','CHAMPION_OF') as $connection)
+            @include('modules.article_media',['article'=>$connection])
+        @endforeach
+       <h1>Members / Agents</h1>
+        @foreach($article->reverseConnections->whereIn('pivot.relationship',
+        ['MEMBER_OF', 'AGENT_OF']) as $connection)
+            @include('modules.article_media',['article'=>$connection])
+        @endforeach
+        @foreach($article->forwardConnections->whereIn('pivot.relationship',
+        ['FACTION_OF']) as $connection)
             @include('modules.article_media',['article'=>$connection])
         @endforeach
     </div>
