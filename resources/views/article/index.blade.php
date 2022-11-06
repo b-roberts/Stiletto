8c01ed654de462163f67d11d9bc0d08c7d380033
Barrek Roberts <b-roberts@users.noreply.github.com>
2022-11-06 08:51:18 -0600

    Reformats homepage

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -7,22 +7,42 @@ Articles
 @section('breadcrumb-menu')
 @endsection
 @section('content')
+
+<div class="nav-tabs-boxed">
+    <ul class="nav nav-tabs" role="tablist">
+        @foreach($conceptArticles->keys() as $concept)
+            <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#tab-{{Str::slug($concept)}}" role="tab" aria-controls="tab-{{Str::slug($concept)}}">{{$concept}}</a>
+            </li>
+        @endforeach
+        <li class="nav-item">
+            <a class="nav-link" data-toggle="tab" href="#tab-graph" role="tab" aria-controls="home">Graph</a>
+        </li>
+    </ul>
+</div>
+<div class="tab-content">
     @foreach($conceptArticles as $concept=>$articles)
-        <h1>{{$concept}}</h1>
-        <div class="card">
-            <div class="card-body">
+        <div class="tab-pane" id="tab-{{Str::slug($concept)}}" role="tabpanel">
+            <h1>{{$concept}}</h1>
+            <div class="row">
                 @foreach($articles as $article)
-                    @include('modules.article_media',['article'=>$article])
+                    <div class="col-12 col-xl-3">
+                        @include('modules.article_media',['article'=>$article])
+                    </div>
                 @endforeach
             </div>
         </div>
     @endforeach
-<script>
-var json = {!!json_encode($graph)!!};
-</script>
-<div id="inner-details"></div>
-    <div id ="infovis" style="height:75vh; background:black; overflow:hidden; "></div>
 
+
+    <script>
+    var json = {!!json_encode($graph)!!};
+    </script>
+    <div class="tab-pane" id="tab-graph" role="tabpanel">
+        <div id="inner-details"></div>
+        <div id ="infovis" style="height:75vh; background:black; overflow:hidden; "></div>
+    </div>
+</div>
     <script src="{{asset('js/test.js')}}"></script>
     <script src="{{asset('js/rgraph.js')}}"></script>
 @endsection
