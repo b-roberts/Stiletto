d1607ffe3feeee31cd1df048a0dd451ac2041853
Barrek Roberts <b-roberts@users.noreply.github.com>
2023-02-25 20:09:37 -0600

    Replace sidebar with blade-x

--- resources/views/left_menu.blade.php
+++ resources/views/left_menu.blade.php
@@ -1,68 +1,24 @@
 <div class="sidebar sidebar-show d-print-none">
     <nav class="sidebar-nav">
         <ul class="nav">
-                <li class="nav-title">
-                    Geography
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Place'])}}">
-                        <i class="nav-icon fas fa-map-marked-alt"></i> Places
-                    </a>
-                </li>
-                <li class="nav-title">
-                    Characters
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Character'])}}">
-                        <i class="nav-icon fas fa-user"></i> Characters
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Player Character'])}}">
-                        <i class="nav-icon fas fa-user-shield"></i> Player Characters
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Group'])}}">
-                        <i class="nav-icon fas fa-users"></i> Groups
-                    </a>
-                </li>
-                <li class="nav-title">
-                    Events
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Adventure'])}}">
-                        <i class="nav-icon fas fa-compass"></i> Adventure
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Session Log'])}}">
-                        <i class="nav-icon fas fa-book"></i> Session Logs
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Event'])}}">
-                        <i class="nav-icon fas fa-stopwatch"></i> Events
-                    </a>
-                </li>
-                <li class="nav-title">
-                    Notes
-                </li>
-				<li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Item'])}}">
-                        <i class="nav-icon fas fa-treasure-chest"></i> Items
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Folder'])}}">
-                        <i class="nav-icon fas fa-folder"></i> Folders
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('concept',['concept'=>'Note'])}}">
-                        <i class="nav-icon fas fa-sticky-note"></i> Notes
-                    </a>
-                </li>
+            <x-nav-group title="Geography">
+                <x-nav-item href="{{route('concept',['concept'=>'Place'])}}" icon="fas fa-map-marked-alt">Places</x-nav-item>
+            </x-nav-group>
+            <x-nav-group title="Characters">
+                <x-nav-item href="{{route('concept',['concept'=>'Character'])}}" icon="fas fa-user">Characters</x-nav-item>
+                <x-nav-item href="{{route('concept',['concept'=>'Player Character'])}}" icon="fas fa-user-shield">Player Characters</x-nav-item>
+                <x-nav-item href="{{route('concept',['concept'=>'Group'])}}" icon="fas fa-users">Groups</x-nav-item>
+            </x-nav-group>
+            <x-nav-group title="Events">
+                <x-nav-item href="{{route('concept',['concept'=>'Adventure'])}}" icon="fas fa-compass">Adventure</x-nav-item>
+                <x-nav-item href="{{route('concept',['concept'=>'Session Log'])}}" icon="fas fa-book">Session Logs</x-nav-item>
+                <x-nav-item href="{{route('concept',['concept'=>'Event'])}}" icon="fas fa-stopwatch">Events</x-nav-item>
+            </x-nav-group>
+            <x-nav-group title="Notes">
+                <x-nav-item href="{{route('concept',['concept'=>'Item'])}}" icon="fas fa-treasure-chest">Items</x-nav-item>
+                <x-nav-item href="{{route('concept',['concept'=>'Folder'])}}" icon="fas fa-folder">Folders</x-nav-item>
+                <x-nav-item href="{{route('concept',['concept'=>'Note'])}}" icon="fas fa-sticky-note">Notes</x-nav-item>
+            </x-nav-group>
         </ul>
     </nav>
 </div>
